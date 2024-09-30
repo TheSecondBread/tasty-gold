1a005f41801bf6c44324c25f95f63a5c4e0ffa7c
TheSecondBread <gamezone2904@gmail.com>
2024-09-30 16:33:12 +0530

    refactored the db insert code

--- backend/internal/handlers/handlers.go
+++ backend/internal/handlers/handlers.go
@@ -37,40 +37,9 @@ func SubmissionHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//process the request, i.e., store it in the db
-	pool := db.GetConnectionPool()
+	result, httpStatus := insertIntoDB(&submitRequest)
 
-	var fetchedCoupon string
-	couponQuery := "SELECT coupon_code FROM coupons WHERE coupon_code = $1"
-
-	err = pool.QueryRow(context.Background(), couponQuery, submitRequest.CouponCode).Scan(&fetchedCoupon)
-
-	if err != nil {
-		if err == pgx.ErrNoRows {
-			//coupon code not found in the table
-			SendJSONResponse(w, map[string]string{"msg": "invalid coupon"}, http.StatusBadRequest)
-			return
-		} else {
-			SendJSONResponse(w, map[string]string{"error": "failed to query the database"}, http.StatusBadRequest)
-			return
-		}
-	}
-
-	if fetchedCoupon == submitRequest.CouponCode {
-		query := "INSERT INTO submissions (name, phone, place, district, pincode, state, coupon_code, comments, image_base_64) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)"
-		_, err = pool.Exec(context.Background(), query, submitRequest.Name, submitRequest.Phone, submitRequest.Place, submitRequest.District, submitRequest.Pincode, submitRequest.State, submitRequest.CouponCode, submitRequest.Comments, submitRequest.ImageBase64)
-
-		if err != nil {
-			// fmt.Println(err)
-			SendJSONResponse(w, map[string]string{"msg": "coupon already exists"}, http.StatusBadRequest)
-			return
-		}
-	} else {
-		SendJSONResponse(w, map[string]string{"msg": "failed to query the database"}, http.StatusBadRequest)
-		return
-	}
-
-	SendJSONResponse(w, map[string]string{"msg": "success"}, http.StatusOK)
+	SendJSONResponse(w, result, httpStatus)
 }
 
 func SendJSONResponse(w http.ResponseWriter, data any, statusCode int) {
@@ -80,4 +49,42 @@ func SendJSONResponse(w http.ResponseWriter, data any, statusCode int) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
+}
+
+//validate everything before insertion
+func insertIntoDB(submitRequest *models.SubmitRequest) (map[string]string, int) {
+	//process the request, i.e., store it in the db
+	pool := db.GetConnectionPool()
+
+	var fetchedCoupon string
+	couponQuery := "SELECT coupon_code FROM coupons WHERE coupon_code = $1"
+
+	err := pool.QueryRow(context.Background(), couponQuery, submitRequest.CouponCode).Scan(&fetchedCoupon)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			//coupon code not found in the table
+			// SendJSONResponse(w, map[string]string{"msg": "invalid coupon"}, http.StatusBadRequest)
+			return map[string]string{"msg": "invalid coupon"}, http.StatusBadRequest
+		} else {
+			// SendJSONResponse(w, map[string]string{"error": "failed to query the database"}, http.StatusBadRequest)
+			return map[string]string{"error": "failed to query the database"}, http.StatusBadRequest
+		}
+	}
+
+	if fetchedCoupon == submitRequest.CouponCode {
+		query := "INSERT INTO submissions (name, phone, place, district, pincode, state, coupon_code, comments, image_base_64) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)"
+		_, err = pool.Exec(context.Background(), query, submitRequest.Name, submitRequest.Phone, submitRequest.Place, submitRequest.District, submitRequest.Pincode, submitRequest.State, submitRequest.CouponCode, submitRequest.Comments, submitRequest.ImageBase64)
+
+		if err != nil {
+			// fmt.Println(err)
+			// SendJSONResponse(w, map[string]string{"msg": "coupon already exists"}, http.StatusBadRequest)
+			return map[string]string{"msg": "coupon already exists"}, http.StatusBadRequest
+		}
+	} else {
+		// SendJSONResponse(w, map[string]string{"msg": "failed to query the database"}, http.StatusBadRequest)
+		return map[string]string{"msg": "failed to query the database"}, http.StatusBadRequest
+	}
+
+	return map[string]string{"msg": "success"}, http.StatusOK
 }
