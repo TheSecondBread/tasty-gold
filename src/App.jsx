14f4af099b289136b3060cd1c3419896ccc93021
TheSecondBread <gamezone2904@gmail.com>
2024-11-03 15:02:31 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import Qr from './components/Qr'
-import Home from './Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Success from './Success'
-import WinnersDashboard from './components/winners/WinnersDashboard'
-import WinnersByWeeks from './components/winners/WinnersByWeeks'
-import PrivacyPolicy from './components/PrivacyPolicy'
-import ViewWinner from './components/winners/ViewWinner'
-import WinnersTable from './components/winners/WinnersTable'
-import TermsAndConditions from './components/TermsAndConditions'
-import AlreadyRegistered from './AlreadyRegistered'
-import Sidebar from './components/Sidebar'
-import GenerateWinners from './components/winners/GenerateWinners'
-
+import Qr from './components/Qr';
+import Home from './Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Success from './Success';
+import WinnersDashboard from './components/winners/WinnersDashboard';
+import WinnersByWeeks from './components/winners/WinnersByWeeks';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import ViewWinner from './components/winners/ViewWinner';
+import WinnersTable from './components/winners/WinnersTable';
+import TermsAndConditions from './components/TermsAndConditions';
+import AlreadyRegistered from './AlreadyRegistered';
+import Sidebar from './components/Sidebar';
+import GenerateWinners from './components/winners/GenerateWinners';
+import Footer from './components/Footer';
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Sidebar></Sidebar>
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path='/qr' element={<Qr></Qr>}></Route>
-      <Route path='/winners-dashboard' element={<WinnersDashboard></WinnersDashboard>}></Route>
-      <Route path='/winners-by-weeks' element={<WinnersByWeeks></WinnersByWeeks>}></Route>
-      <Route path='/winners' element={<WinnersTable></WinnersTable>}></Route>
-      <Route path='/privacy-policy' element={<PrivacyPolicy></PrivacyPolicy>}></Route>
-      <Route path='/terms-and-conditions' element={<TermsAndConditions></TermsAndConditions>}></Route>
-
-      <Route path='/view-winner' element={<ViewWinner></ViewWinner>}></Route>
-      <Route path='/success' element={<Success></Success>}></Route>
-      <Route path='/already-registered' element={<AlreadyRegistered></AlreadyRegistered>}></Route>
-
-      <Route path='/generate-winners' element={<GenerateWinners></GenerateWinners>}></Route>
-
-
-    </Routes>
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/qr" element={<Qr />} />
+            <Route path="/winners-dashboard" element={<WinnersDashboard />} />
+            <Route path="/winners-by-weeks" element={<WinnersByWeeks />} />
+            <Route path="/winners" element={<WinnersTable />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+            <Route path="/view-winner" element={<ViewWinner />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/already-registered" element={<AlreadyRegistered />} />
+            <Route path="/generate-winners" element={<GenerateWinners />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className=" text-white text-center mt-20 px-4 bg-[#be3f1c] py-1 text-[12px]">
+      <div className="flex justify-center gap-1">
+        <p>
+        Developed by: Bug Breakers | 
+        </p>
+        <a href="mailto:bugbreakers11@gmail.com" className="font-bold underline">
+           Contact Us
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
